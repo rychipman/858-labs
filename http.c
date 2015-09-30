2caff478fdc2d459038f9bd9142d9912b9a6d3e8
Ryan Chipman <ryan@ryanchipman.com>
2015-09-30 15:36:17 +0000

    fixup! fix overflow bugs 1 and 2

--- http.c
+++ http.c
@@ -102,7 +102,7 @@ const char *http_request_line(int fd, char *reqpath, char *env, size_t *env_len)
     }
 
     /* decode URL escape sequences in the requested path into reqpath */
-    url_decode(reqpath, sp1);
+    url_decode(reqpath, sp1, 2048);
 
     envp += sprintf(envp, "REQUEST_URI=%s", reqpath) + 1;
 
@@ -156,7 +156,7 @@ const char *http_request_headers(int fd)
         }
 
         /* Decode URL escape sequences in the value */
-        url_decode(value, sp);
+        url_decode(value, sp, 512);
 
         /* Store header in env. variable for application code */
         /* Some special headers don't use the HTTP_ prefix. */
@@ -434,9 +434,9 @@ void http_serve_executable(int fd, const char *pn)
     }
 }
 
-void url_decode(char *dst, const char *src)
+void url_decode(char *dst, const char *src, size_t len)
 {
-    for (;;)
+    for (int i=0;i<len;i++)
     {
         if (src[0] == '%' && src[1] && src[2])
         {
@@ -460,9 +460,6 @@ void url_decode(char *dst, const char *src)
 
             if (*dst == '\0')
                 break;
-        }
-        if strlen(dst) == 0 {
-            break;
         }
         dst++;
     }
